6f61a938f744aab7507e96248c7c59986f2c5de3
VortexZaa <abidinayhan94@gmail.com>
2024-01-10 04:56:53 +0300

    update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,7 +4,7 @@ import Logo from "./icon/logo";
 export default function Footer() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center py-10">
+      <div className="flex flex-col items-center justify-center pb-6 md:py-6 xl:py-10">
         <div className="w-full max-w-[1400px] flex flex-col justify-center items-center gap-3 md:gap-6 h-fit   border-white/25 border rounded-[20px] py-4 md:py-6 card-bg z-0 px-3">
           <Link
             href={"/"}
