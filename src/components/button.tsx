c96e5985de84bac6e5bf744a71c13209763cf230
eminsamur <eminsamur00@gmail.com>
2024-01-12 19:02:47 +0300

    download

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,20 +1,20 @@
 export default function NewButton() {
   return (
-    <div className="flex flex-col md:flex-row justify-center gap-6 md:gap-10 items-center w-full ">
+    <div className="grid grid-cols-1 md:grid-cols-3 justify-center gap-6 md:gap-10 items-center w-full ">
       {buttonData.map((item) => (
         <div
           key={item?.id}
           className="flex flex-col items-center justify-center gap-3 w-full"
         >
-          <p className="text-center text-sm md:text-2xl">
-            {item?.title}
-          </p>
+          <p className="text-center text-sm md:text-2xl">{item?.title}</p>
 
           <button className="w-2/3 md:w-[250px] mt-2 md:mt-5 bg-white/[0.023] rounded-lg backdrop-blur-sm border-white px-2 py-3 border-[1px] text-base md:text-lg font-medium text-white  items-center bg-none text-center flex gap-3 justify-center">
-            <p className="text-xs md:text-lg">
-              {item?.description}
-            </p>
-            <img className="w-8 h-8 md:w-12 md:h-12" src={item?.svg} alt="" />
+            <p className="text-xs md:text-lg">{item?.description}</p>
+            <img
+              className={` ${item?.addClass} w-6 h-6 md:w-12 md:h-12`}
+              src={item?.svg}
+              alt=""
+            />
           </button>
         </div>
       ))}
@@ -28,20 +28,22 @@ const buttonData = [
     title: "Windows",
     description: "DOWNLOAD",
     svg: "/images/icons/ri_windows-fill2.svg",
-    link: "#",
+    link: "https://github.com/datsproject/dats-desktop-publish/releases/download/v1.0.7/Dats-Project-Setup-1.0.7.exe",
   },
   {
     id: 2,
     title: "MAC OS M Series Chip",
     description: "DOWNLOAD",
+    addClass: "mb-1.5",
     svg: "/images/icons/ic_outline-apple.svg",
-    link: "#",
+    link: "https://github.com/datsproject/dats-desktop-publish/releases/download/v1.0.7/Dats-Project-1.0.7-arm64.dmg",
   },
   {
     id: 3,
-    title: "MAC OS Intel Chip",
+    title: "MAC OS X64",
     description: "DOWNLOAD",
+    addClass: "mb-1.5",
     svg: "/images/icons/ic_outline-apple.svg",
-    link: "#",
+    link: "https://github.com/datsproject/dats-desktop-publish/releases/download/v1.0.7/Dats-Project-1.0.7.dmg",
   },
 ];
