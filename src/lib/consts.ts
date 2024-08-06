aa9013277611d5739d28629ad97ec3aafcc15178
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 02:24:53 +0300

    chore: Add NAVBARMENU constant to consts.ts

---
+++ src/lib/consts.ts
@@ -0,0 +1,24 @@
+import { MenuItem } from "./types";
+
+export const NAVBARMENU = [
+  {
+    name: "How it works",
+    href: "#",
+    id: 1,
+  },
+  {
+    name: "About DATS",
+    href: "#",
+    id: 2,
+  },
+  {
+    name: "FAQ",
+    href: "#",
+    id: 3,
+  },
+  {
+    name: "Contact",
+    href: "#",
+    id: 4,
+  },
+] as MenuItem[];
