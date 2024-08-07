00c1e58731f0a3c94872d22e19e2229b50eafd43
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 06:37:23 +0300

    feat: Add TollIcon component to cta-how folder

---
+++ src/components/cta-how/toll-icon.tsx
@@ -0,0 +1,41 @@
+const TollIcon = () => {
+    return (
+        <svg width="106" height="90" viewBox="0 0 106 90" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d_90_125)">
+                <mask id="mask0_90_125" style={{ maskType: "alpha" }} maskUnits="userSpaceOnUse" x="22" y="12" width="62" height="62">
+                    <rect x="22" y="12" width="62" height="62" fill="url(#paint0_linear_90_125)" />
+                </mask>
+                <g mask="url(#mask0_90_125)">
+                    <path d="M60.75 63.6666C54.9806 63.6666 50.0938 61.6645 46.0896 57.6603C42.0854 53.6562 40.0833 48.7694 40.0833 42.9999C40.0833 37.2305 42.0854 32.3437 46.0896 28.3395C50.0938 24.3353 54.9806 22.3333 60.75 22.3333C66.5195 22.3333 71.4063 24.3353 75.4104 28.3395C79.4146 32.3437 81.4167 37.2305 81.4167 42.9999C81.4167 48.7694 79.4146 53.6562 75.4104 57.6603C71.4063 61.6645 66.5195 63.6666 60.75 63.6666ZM40.0833 63.0208C35.5195 61.8152 31.7951 59.361 28.9104 55.6583C26.0257 51.9555 24.5833 47.736 24.5833 42.9999C24.5833 38.2638 26.0257 34.0444 28.9104 30.3416C31.7951 26.6388 35.5195 24.1846 40.0833 22.9791V28.4041C36.9833 29.4805 34.4861 31.3534 32.5917 34.0228C30.6972 36.6923 29.75 39.6846 29.75 42.9999C29.75 46.3152 30.6972 49.3076 32.5917 51.977C34.4861 54.6464 36.9833 56.5194 40.0833 57.5958V63.0208Z" fill="url(#paint1_linear_90_125)" />
+                </g>
+            </g>
+            <defs>
+                <filter id="filter0_d_90_125" x="0.497013" y="0.0310942" width="105.006" height="89.5059" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                    <feOffset dy="1.78417" />
+                    <feGaussianBlur stdDeviation="12.0432" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix type="matrix" values="0 0 0 0 0.172549 0 0 0 0 0.878431 0 0 0 0 0.996078 0 0 0 0.8 0" />
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_90_125" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_90_125" result="shape" />
+                </filter>
+                <linearGradient id="paint0_linear_90_125" x1="53" y1="-153.366" x2="53.0003" y2="74" gradientUnits="userSpaceOnUse">
+                    <stop offset="2.71717e-07" stopColor="#0A2141" />
+                    <stop offset="0.224894" stopColor="#003D7A" />
+                    <stop offset="0.633565" stopColor="#1791CE" />
+                    <stop offset="1" stopColor="#2CE1FE" />
+                </linearGradient>
+                <linearGradient id="paint1_linear_90_125" x1="53" y1="-87.9107" x2="53.0001" y2="63.6666" gradientUnits="userSpaceOnUse">
+                    <stop offset="2.71717e-07" stopColor="#0A2141" />
+                    <stop offset="0.224894" stopColor="#003D7A" />
+                    <stop offset="0.633565" stopColor="#1791CE" />
+                    <stop offset="1" stopColor="#2CE1FE" />
+                </linearGradient>
+            </defs>
+        </svg>
+
+    )
+}
+
+export default TollIcon
