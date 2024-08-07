6200cf027d2f336c8147d302999d24258bec7e19
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 03:46:46 +0300

    chore: Update intro section layout and styles

--- src/components/intro/index.tsx
+++ src/components/intro/index.tsx
@@ -1,7 +1,8 @@
+import RightArrow from "../common/right-arrow"
 import Sphere from "./sphere"
 const IntroSection = () => {
     return (
-        <section className="flex items-center justify-center h-screen  md:gap-20">
+        <section className="flex items-center justify-between h-screen  ">
             <div className="flex flex-col items-start gap-10 justify-center max-w-xl ">
                 <h3 className="text-[14px] font-semibold text-center text-transparent bg-clip-text bg-gradient-to-l from-[#0A2141] to-[#55FFFA] border-[1.5px] border-white/15 rounded-full px-[14px] py-[4px]" >
                     START EARNING TODAY
@@ -9,9 +10,9 @@ const IntroSection = () => {
                 <h1 className="font-normal text-[#] text-[83px] tracking-tight leading-[1.04]">
                     Monetize your idle computing power <span className="text-[#2CE1FE]">securely</span>
                 </h1>
-                <div className="flex items-center justify-between w-full">
-                    <button className="flex items-center justify-center bg-[#EAEAEA] text-base text-black font-semibold rounded-full py-[10px] px-[22px] ">
-                        Get Started Today <span >→</span>
+                <div className="flex items-center justify-between w-full pr-20">
+                    <button className="flex items-center justify-center gap-4 bg-[#EAEAEA] text-base text-black font-semibold rounded-full py-[10px] px-[22px] ">
+                        Get Started Today <RightArrow color="black" />
                     </button>
                     <h4 className="w-[240px] font-normal text-[#f0f0f0bb] ">
                         A secure Web3 experience <br /> while earning passive income
