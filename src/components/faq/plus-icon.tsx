c0e6873af8862e059ec4bd3c047150cbcaf17c26
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 08:16:38 +0300

    feat: Add PlusIcon component for FAQ section

---
+++ src/components/faq/plus-icon.tsx
@@ -0,0 +1,11 @@
+const PlusIcon = () => {
+    return (
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 6L12 18" stroke="#F0F0F0" strokeWidth="2" strokeLinecap="round" />
+            <path d="M18 12L6 12" stroke="#F0F0F0" strokeWidth="2" strokeLinecap="round" />
+        </svg>
+
+    )
+}
+
+export default PlusIcon
