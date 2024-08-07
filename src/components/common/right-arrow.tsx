f11d38efa5fa55c50450b3aebed1054d037e8aa5
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 04:39:35 +0300

    refactor: Update RightArrow component stroke attributes

--- src/components/common/right-arrow.tsx
+++ src/components/common/right-arrow.tsx
@@ -6,7 +6,7 @@ type RightArrowProps = {
 const RightArrow: React.FC<RightArrowProps> = ({ color }) => {
     return (
         <svg width="14" height="11" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M8.33333 0.833496L13 5.50016M13 5.50016L8.33333 10.1668M13 5.50016L1 5.50016" stroke={color} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M8.33333 0.833496L13 5.50016M13 5.50016L8.33333 10.1668M13 5.50016L1 5.50016" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
         </svg>
 
     )
