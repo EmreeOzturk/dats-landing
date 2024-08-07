41ff45d77c25c20fb45b6d6f51b08f354fa7b35a
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 06:37:49 +0300

    feat: Add LeftSphere component to why folder

---
+++ src/components/why/left-sphere.tsx
@@ -0,0 +1,68 @@
+const Sphere = () => {
+    return (
+        <svg width="889" height="799" viewBox="0 0 889 799" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_f_0_1)">
+                <path d="M563.679 340.927C477.739 396.882 493.086 536.888 328.762 536.888C230.315 536.888 150.508 457.081 150.508 358.634C150.508 231.142 230.315 180.381 328.762 180.381C495.683 146.146 543.847 231.141 563.679 340.927Z" fill="url(#paint0_radial_0_1)" />
+            </g>
+            <g filter="url(#filter1_f_0_1)">
+                <ellipse cx="273.918" cy="416.932" rx="84.2834" ry="41.0782" transform="rotate(43.4939 273.918 416.932)" fill="#55FFFA" fillOpacity="0.9" />
+            </g>
+            <g filter="url(#filter2_bf_0_1)">
+                <path d="M175.597 71.7764L817.246 71.7764V634.633L704.771 728.091L399.589 558.137L300.428 523.667L243.605 413.794C240.364 407.527 234.489 403.037 227.59 401.556L71.6497 368.079L175.597 71.7764Z" fill="url(#paint1_linear_0_1)" fillOpacity="0.01" />
+            </g>
+            <g filter="url(#filter3_f_0_1)">
+                <circle cx="281.538" cy="408.215" r="103.647" fill="black" />
+            </g>
+            <g filter="url(#filter4_f_0_1)">
+                <path d="M322.023 505.949C308.525 519.447 242.206 523.892 204.044 485.73C165.882 447.568 170.327 381.249 183.825 367.751C197.323 354.253 229.801 383.648 267.964 421.81C306.126 459.973 335.521 492.451 322.023 505.949Z" fill="url(#paint2_linear_0_1)" fillOpacity="0.5" />
+            </g>
+            <defs>
+                <filter id="filter0_f_0_1" x="148.147" y="170.341" width="417.893" height="368.908" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="1.18049" result="effect1_foregroundBlur_0_1" />
+                </filter>
+                <filter id="filter1_f_0_1" x="135.709" y="280.873" width="276.418" height="272.12" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="35.4146" result="effect1_foregroundBlur_0_1" />
+                </filter>
+                <filter id="filter2_bf_0_1" x="0.820381" y="0.94709" width="887.255" height="797.973" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feGaussianBlur in="BackgroundImageFix" stdDeviation="21.2488" />
+                    <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_0_1" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_0_1" result="shape" />
+                    <feGaussianBlur stdDeviation="35.4146" result="effect2_foregroundBlur_0_1" />
+                </filter>
+                <filter id="filter3_f_0_1" x="177.419" y="304.096" width="208.238" height="208.238" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="0.236098" result="effect1_foregroundBlur_0_1" />
+                </filter>
+                <filter id="filter4_f_0_1" x="126.875" y="317.199" width="245.701" height="245.7" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape" />
+                    <feGaussianBlur stdDeviation="23.6098" result="effect1_foregroundBlur_0_1" />
+                </filter>
+                <radialGradient id="paint0_radial_0_1" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(196.547 476.683) rotate(-38.496) scale(257.922 257.922)">
+                    <stop stopColor="#55FFFA" />
+                    <stop offset="0.370519" stopColor="#0097D3" />
+                    <stop offset="1" stopColor="#0A2141" />
+                </radialGradient>
+                <linearGradient id="paint1_linear_0_1" x1="589.089" y1="296.064" x2="194.049" y2="471.973" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#0A2141" />
+                    <stop offset="0.5" stopColor="#00DDDD" />
+                    <stop offset="1" stopColor="#C7C7C7" />
+                </linearGradient>
+                <linearGradient id="paint2_linear_0_1" x1="200.796" y1="479.28" x2="263.598" y2="425.45" gradientUnits="userSpaceOnUse">
+                    <stop stopColor="#55FFFA" />
+                    <stop offset="1" stopColor="#55FFFA" stopOpacity="0" />
+                </linearGradient>
+            </defs>
+        </svg>
+
+
+    )
+}
+
+export default Sphere
