4ed80f05cafffb76e1c10d58fdc06e623ee4686b
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 08:16:14 +0300

    refactor: Update Home page layout and components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import BrandsSection from "@/components/brand";
 import CTASection from "@/components/cta-how";
+import FAQSection from "@/components/faq";
+import HowSection from "@/components/how";
 import IntroSection from "@/components/intro";
+import SomeDetails from "@/components/some-details";
 import WhySection from "@/components/why";
+import WhyUsSection from "@/components/why-us";
 
 export default function Home() {
   return (
-    <main className="">
+    <main >
       <IntroSection />
       <BrandsSection />
       <WhySection />
       <CTASection />
+      <HowSection />
+      <WhyUsSection />
+      <SomeDetails />
+      <FAQSection />
     </main>
   );
 }
