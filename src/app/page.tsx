84291b9808d25721f0d9d5bba77e0ce9b9946b07
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 06:37:39 +0300

    feat: Add WhySection and CTASection components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import BrandsSection from "@/components/brand";
+import CTASection from "@/components/cta-how";
 import IntroSection from "@/components/intro";
+import WhySection from "@/components/why";
 
 export default function Home() {
   return (
     <main className="">
       <IntroSection />
       <BrandsSection />
+      <WhySection />
+      <CTASection />
     </main>
   );
 }
