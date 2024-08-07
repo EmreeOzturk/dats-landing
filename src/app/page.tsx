fa8d65f1d8bb571a4eae0a4def1fe40a82e3e8a8
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 03:47:03 +0300

    feat: Add BrandsSection component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import BrandsSection from "@/components/brand";
 import IntroSection from "@/components/intro";
 
 export default function Home() {
   return (
     <main className="">
       <IntroSection />
+      <BrandsSection />
     </main>
   );
 }
