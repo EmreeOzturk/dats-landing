c86a9e6bfd0192ffdbdef94ad1ebd2f355cb0a24
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 03:46:49 +0300

    chore: Update navbar layout and styles

--- src/components/header/navbar.tsx
+++ src/components/header/navbar.tsx
@@ -1,17 +1,24 @@
 import { NAVBARMENU } from "@/lib/consts";
+import RightArrow from "../common/right-arrow";
 const Navbar = () => {
     return (
-        <div className="w-full flex items-center justify-between">
-            <ul className="flex items-center justify-center gap-10">
+        <div className="w-full flex items-center justify-between ">
+            <ul className="flex items-center justify-center gap-10 text-[#f0f0f098] ">
                 {NAVBARMENU.map((item) => (
-                    <li key={item.id}>
+                    <li key={item.id} className="hover:text-white font-normal transition-all">
                         {item.name}
                     </li>
                 ))}
             </ul>
-            <div>
-                <button className="bg-white text-black">Sign Up</button>
-                <button>Download</button>
+            <div className="flex items-center justify-center gap-8">
+                <button className="bg-white text-black rounded-full font-semibold py-[10px] px-[22px] flex items-center justify-center gap-4">
+                    dApps
+                    <RightArrow color="black" />
+                </button>
+                <button className="text-white border border-white/15 rounded-full py-[10px] px-[22px] flex items-center justify-center gap-4">
+                    Download
+                    <RightArrow color="white" />
+                </button>
             </div>
         </div>
     )
