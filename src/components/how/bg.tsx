9f1280822d89fe8b337980606f6c66d6c9151e9a
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 08:16:40 +0300

    refactor: Add BGOverlay component for background in 'how' section

---
+++ src/components/how/bg.tsx
@@ -0,0 +1,11 @@
+const BGOverlay = () => {
+    return (
+        <svg width="971" height="971" viewBox="0 0 971 971" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="485.5" cy="485.5" r="480" stroke="white" strokeOpacity="0.5" />
+            <circle opacity="0.16" cx="485.5" cy="485.5" r="485" stroke="#F0F0F0" />
+            <circle cx="485.5" cy="485.5" r="345.5" fill="#060606" />
+        </svg>
+    )
+}
+
+export default BGOverlay
