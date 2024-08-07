0eb40c5cd2d8cd95ae86702163917477550d77fe
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 03:46:55 +0300

    chore: Update header layout and styles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import Logo from './logo'
 import Navbar from './navbar'
 const Header = () => {
     return (
-        <header className='flex gap-10 px-10 pt-8'>
+        <header className='flex items-center absolute top-0 left-0 right-0 justify-between gap-10  pt-8 max-w-[1440px] mx-auto '>
             <Logo />
             <Navbar />
         </header>
