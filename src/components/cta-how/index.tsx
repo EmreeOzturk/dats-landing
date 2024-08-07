6f5150a89531a37a99caf7180780b930776f1778
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 06:37:19 +0300

    refactor: Add CTASection component to cta-how folder

---
+++ src/components/cta-how/index.tsx
@@ -0,0 +1,17 @@
+import StepCard from "./step-card"
+
+const CTASection = () => {
+    return (
+        <section className="flex flex-col items-start justify-between overh py-32  max-w-7xl mx-auto ">
+            <h2 className="font-normal text-[48px] tracking-tight leading-snug">Start your journey in under one minute</h2>
+            <div className="flex items-center justify-start w-full gap-4 pt-16">
+                <StepCard step={1} title="step 1" />
+                <StepCard step={2} title="step 2" />
+                <StepCard step={3} title="step 3" />
+                <StepCard step={4} title="step 4" />
+            </div>
+        </section>
+    )
+}
+
+export default CTASection
