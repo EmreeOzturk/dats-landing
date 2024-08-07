1ecbd0aa19a1f1853afd70cc61c56b20aff7feaa
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 08:44:10 +0300

    refactor: Update FAQSection component layout

--- src/components/faq/index.tsx
+++ src/components/faq/index.tsx
@@ -2,7 +2,7 @@ import Faqs from "./faqs"
 
 const FAQSection = () => {
     return (
-        <section className="flex flex-col gap-6 items-center justify-between h-[110dvh] overflow-hidden max-w-7xl mx-auto pb-32 ">
+        <section className="flex flex-col  items-center justify-between gap-6 overflow-hidden max-w-7xl mx-auto pb-32">
             <h3 className="text-[14px] font-semibold text-center text-transparent bg-clip-text bg-gradient-to-l from-[#0A2141] to-[#55FFFA] border-[1.5px] border-white/15 rounded-full px-[14px] py-[4px]" >
                 GOT QUESTIONS?
             </h3>
