168e5e21a67dd7244798cade83a7d55dee4b552d
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-07 06:37:26 +0300

    feat: Add ChartIcon component to cta-how folder

---
+++ src/components/cta-how/chart-icon.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const ChartIcon = () => {
+    return (
+        <svg width="120" height="104" viewBox="0 0 120 104" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g filter="url(#filter0_d_90_110)">
+                <mask id="mask0_90_110" style={{ maskType: "alpha" }} maskUnits="userSpaceOnUse" x="22" y="14" width="73" height="73">
+                    <rect x="22" y="14" width="72.9024" height="72.9024" fill="#D9D9D9" />
+                </mask>
+                <g mask="url(#mask0_90_110)">
+                    <path d="M32.3278 70.1958L28.0752 65.7913L50.8572 43.0093L63.0076 55.1597L71.8167 45.2875C69.2347 42.2499 66.1971 39.8451 62.7039 38.0732C59.2106 36.3013 55.363 35.4153 51.161 35.4153C47.5665 35.4153 44.1745 36.0861 40.985 37.4277C37.7955 38.7693 34.9098 40.5792 32.3278 42.8574L28.0752 38.4529C31.2141 35.6178 34.7326 33.3902 38.6309 31.7702C42.5291 30.1501 46.7058 29.3401 51.161 29.3401C56.1224 29.3401 60.7041 30.34 64.9061 32.3397C69.1081 34.3395 72.7786 37.1366 75.9174 40.7311L84.5746 30.8589L88.8272 35.2634L79.7144 45.5913C81.3851 48.2745 82.7394 51.1855 83.7772 54.3244C84.8151 57.4632 85.4859 60.7793 85.7896 64.2725H79.7144C79.4107 61.7412 78.8917 59.3237 78.1577 57.0202C77.4236 54.7167 76.4743 52.5271 75.3099 50.4514L63.3114 63.9687L50.8572 51.6665L32.3278 70.1958Z" fill="url(#paint0_linear_90_110)" />
+                </g>
+            </g>
+            <defs>
+                <filter id="filter0_d_90_110" x="-2.70397" y="0.840852" width="122.31" height="102.414" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
+                    <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                    <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
+                    <feOffset dy="2.27994" />
+                    <feGaussianBlur stdDeviation="15.3896" />
+                    <feComposite in2="hardAlpha" operator="out" />
+                    <feColorMatrix type="matrix" values="0 0 0 0 0.172549 0 0 0 0 0.878431 0 0 0 0 0.996078 0 0 0 0.8 0" />
+                    <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_90_110" />
+                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_90_110" result="shape" />
+                </filter>
+                <linearGradient id="paint0_linear_90_110" x1="58.4512" y1="-79.63" x2="58.4513" y2="70.1958" gradientUnits="userSpaceOnUse">
+                    <stop offset="2.71717e-07" stopColor="#0A2141" />
+                    <stop offset="0.224894" stopColor="#003D7A" />
+                    <stop offset="0.633565" stopColor="#1791CE" />
+                    <stop offset="1" stopColor="#2CE1FE" />
+                </linearGradient>
+            </defs>
+        </svg>
+
+    )
+}
+
+export default ChartIcon
