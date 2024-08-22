154a826fa6fb36b64f36faa2e47037df11f70f84
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-22 12:59:12 +0300

    feat: Add user agreement page

---
+++ src/app/user-agreement/page.tsx
@@ -0,0 +1,122 @@
+import React from 'react'
+
+const UseragreementPage = () => {
+    return (
+
+        <section className="flex items-center justify-between  max-w-7xl mx-auto pt-12">
+
+            <div className="w-full gap-5 grid pt-28">
+                <h1 className="text-left text-5xl ">USER AGREEMENT</h1>
+                <h4 className="font-bold text-base"> Dated: April 2nd, 2023</h4>
+                <h4 className="font-bold text-2xl">Welcome to DATS Project</h4>
+
+                <p>
+                    This User Agreement (“Agreement“) is a contract between DATS
+                    Project (“DATS” or “we” or “us“) and you (the “User” or “you“).
+                    Our platform is designed to provide cybersecurity-focused
+                    decentralized computing power marketplace with a Share-to-Earn
+                    model. By accessing or using our website at www.datsproject.io,
+                    including but not limited to downloading and/or using of DATS
+                    Project’s desktop application, (the “Website“), you agree to be
+                    bound by the terms and conditions of this Agreement.
+                </p>
+                <h4 className="font-bold text-2xl">Acceptance of Terms</h4>
+                <p>
+                    You accept this Agreement, as well as all terms, conditions, and
+                    policies referred to herein or accessible through the Website, by
+                    using the Website. You must immediately stop using the Website if
+                    you disagree with any of the terms or conditions of this
+                    Agreement.
+                </p>
+                <h4 className="font-bold text-2xl">User Conduct</h4>
+                <p>
+                    As a User, you undertake to use the Website in accordance with all
+                    applicable laws and regulations in your region. You commit not to
+                    use the Website in any way that violates the rights of others or
+                    serves any illegal or abusive purpose.
+                </p>
+                <h4 className="font-bold text-2xl">Ownership of Content</h4>
+                <p>
+                    The Website is owned by DATS or its licensors and is protected by
+                    intellectual property laws. This includes all trademarks, text,
+                    graphics, logos, photos, and other content. Without our prior
+                    written approval, you may not utilize any of the Website’s
+                    material.
+                </p>
+                <h4 className="font-bold text-2xl">Disclaimer of Warranty</h4>
+                <p>
+                    The Website is made available to you “as is” and “as available.”
+                    DATS gives no guarantees or representations of any kind, either
+                    stated or implied, as to the Website’s functionality or the
+                    accuracy or reliability of the information, materials, or products
+                    provided on the Website. DATS disclaims all guarantees, express or
+                    implicit, including but not limited to implied warranties of
+                    merchantability and fitness for a particular purpose, to the
+                    fullest extent permitted by applicable law.
+                </p>
+                <h4 className="font-bold text-2xl">Limitation of Liability</h4>
+                <p>
+                    DATS is not responsible for any damages of any kind, including but
+                    not limited to direct, indirect, incidental, punitive, and
+                    consequential damages, that may result from the use of the
+                    website. DATS shall not be responsible for any damages resulting
+                    from interruption, suspension, or termination of the Website under
+                    any circumstance.
+                </p>
+                <h4 className="font-bold text-2xl">Indemnification</h4>
+                <p>
+                    To the fullest extent permitted by applicable law, you will
+                    indemnify and hold harmless DATS, its affiliates, officers,
+                    directors, employees, agents and/or licensors against any claims,
+                    liabilities, damages, awards, judgments, losses, liabilities,
+                    obligations, penalties, fees, expenses (including, without
+                    limitation, attorneys’ fees) and costs in connection with: <br />
+                    Your violation of the Agreement; <br /> Your use or misuse of the
+                    Website and/or the DATS services; <br />
+                    Your violation of any third-party rights or your obligations to
+                    any third party; <br />
+                    Your violation of any applicable law, regulation, or rule; and/or
+                    your negligence, gross negligence and/or willful misconduct.
+                </p>
+                <h4 className="font-bold text-2xl">
+                    Governing Law and Jurisdiction
+                </h4>
+                <p>
+                    This Agreement shall be governed by and construed in accordance
+                    with the laws of the Republic of United States without regard to
+                    its conflict of laws provisions. Any legal action or proceeding
+                    resulting from or connected to this Agreement must be filed in the
+                    Republic of United States state or federal courts.
+                </p>
+                <h4 className="font-bold text-2xl">
+                    Changes to the Agreement and Website
+                </h4>
+                <p>
+                    DATS reserves the right to modify, correct, amend, enhance,
+                    improve, or discontinue this Website and the Agreement without
+                    notice. You also agree that the content on this Website may be
+                    changed, extended, or removed without notice. You are deemed to
+                    have accepted the modified Agreement and Website if you keep using
+                    the Website after any such modification. You agree that DATS is
+                    not liable to you or any third party for any Website modification,
+                    suspension, or discontinuation. You agree that DATS is not liable
+                    for changes-related errors or malfunctions.
+                </p>
+                <h4 className="font-bold text-2xl">Entire Agreement</h4>
+                <p>
+                    This Agreement constitutes the entire agreement between you and
+                    DATS with respect to the subject matter hereof and supersedes all
+                    prior or contemporaneous communications and proposals, whether
+                    oral or written, between you and DATS. If you have any questions
+                    about this Agreement, please contact us at the information
+                    specified
+                </p>
+                <h4 className="font-bold text-2xl">DATS Web3 Solutions, Inc</h4>
+                <p>Address: 8 THE GRN STE D DOVER, DE 19901 USA</p>
+                <p>E-mail: legal@datsproject.io</p>
+            </div>
+        </section>
+    )
+}
+
+export default UseragreementPage
