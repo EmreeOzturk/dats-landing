ae2519ac444caf0b2ae0b594e402aa2b67830df6
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-22 12:41:07 +0300

    feat: Add link to homepage in Header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,12 @@
+import Link from 'next/link'
 import Logo from './logo'
 import Navbar from './navbar'
 const Header = () => {
     return (
         <header className='flex items-center absolute top-0 left-0 right-0 justify-between gap-10  pt-8 max-w-[1440px] mx-auto '>
-            <Logo />
+            <Link href='/'>
+                <Logo />
+            </Link>
             <Navbar />
         </header>
     )
