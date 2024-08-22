30af8600de5e48fdecae63035cbcc935f82f44a5
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-22 12:50:47 +0300

    feat: Add social media links to Footer component

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,7 @@
 import { FaXTwitter, FaTelegram, FaMedium, FaLinkedinIn, FaDiscord } from "react-icons/fa6";
 import { FiGithub } from "react-icons/fi";
 import { MdOutlineMail } from "react-icons/md";
+import Link from "next/link";
 import Logo from "../header/logo";
 const Footer = () => {
     return (
@@ -29,13 +30,27 @@ const Footer = () => {
                         <h5 className="text-[#f0f0f098]">STAY UP TO DATE</h5>
                         <div className="flex flex-col items-start justify-start gap-4 w-full mt-12">
                             <div className="flex items-center justify-start gap-4 w-full">
-                                <FaXTwitter size={22} />
-                                <MdOutlineMail size={22} />
-                                <FaTelegram size={22} />
-                                <FiGithub size={22} />
-                                <FaMedium size={22} />
-                                <FaLinkedinIn size={22} />
-                                <FaDiscord size={22} />
+                                <Link href='https://x.com/DATSProject'>
+                                    <FaXTwitter size={24} />
+                                </Link>
+                                <Link href='https://t.me/datsprojectofficial'>
+                                    <FaTelegram size={24} />
+                                </Link>
+                                <Link href='https://medium.com/@dats_project'>
+                                    <FaMedium size={24} />
+                                </Link>
+                                <Link href='https://www.linkedin.com/company/dats-project'>
+                                    <FaLinkedinIn size={24} />
+                                </Link>
+                                <Link href='https://discord.gg/datsproject'>
+                                    <FaDiscord size={24} />
+                                </Link>
+                                <Link href='https://github.com/datsproject' >
+                                    <FiGithub size={24} />
+                                </Link>
+                                <Link href='/' >
+                                    <MdOutlineMail size={24} />
+                                </Link>
                             </div>
                             <input type="email" placeholder="Enter email" className="w-full bg-[#1e1e1e] text-[#f0f0f0] rounded-xl py-4 px-6" />
                             <button className="flex items-center justify-center w-full gap-4 mt-4 bg-[#EAEAEA] text-base text-black font-semibold rounded-full py-[10px] px-[22px] ">
