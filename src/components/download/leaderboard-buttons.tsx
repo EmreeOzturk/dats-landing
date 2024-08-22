0e9581b68b5e7aaed28178b0000303186663975c
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-22 13:33:19 +0300

    feat: Update leaderboard buttons to use global state for selected time period

--- src/components/download/leaderboard-buttons.tsx
+++ src/components/download/leaderboard-buttons.tsx
@@ -1,10 +1,9 @@
 "use client";
 import { motion } from "framer-motion"
-import React from "react";
-const LeaderboardButtons = (
-    { selected, setSelected }: { selected: string, setSelected: React.Dispatch<React.SetStateAction<string>> }
-) => {
-
+import { useLeaderBoardStore } from "@/store/useLeaderBoardStore";
+const LeaderboardButtons = () => {
+    const setSelected = useLeaderBoardStore(state => state.setSelected)
+    const selected = useLeaderBoardStore(state => state.selected)
     return (
         <div className="flex items-center justify-center gap-44 py-6">
             <div className="relative">
@@ -23,12 +22,12 @@ const LeaderboardButtons = (
             </div>
             <div className="relative">
                 {
-                    selected === "This Week" && <motion.span
+                    selected === "All Time" && <motion.span
                         layoutId="underline"
                         className="w-full absolute h-1 bg-white top-full left-0  rounded-b-lg" />
                 }
                 <button
-                    onClick={() => setSelected("This Week")}
+                    onClick={() => setSelected("All Time")}
                     className="text-zinc-300 hover:bg-zinc-300/20 rounded-t-lg transition-all  py-[10px] px-[44px] text-2xl font-thin">
                     All Time</button>
             </div>
