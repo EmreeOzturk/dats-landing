76064e50b5cbb51e1b8bdf85073fc2e8732350bd
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-22 13:33:32 +0300

    refactor: Update loading spinner component with margin-top

--- src/components/download/loading-spinner.tsx
+++ src/components/download/loading-spinner.tsx
@@ -1,7 +1,7 @@
 
 const Spinner = () => {
     return (
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center mt-24">
             <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-500"></div>
         </div>
     )
