d04265265a201812c56f08482fdabf25c9bea877
Emre Ozturk <emreeozturk42@gmail.com>
2024-08-22 12:45:57 +0300

    refactor: Add id attribute to HowSection for anchor link functionality

--- src/components/how/index.tsx
+++ src/components/how/index.tsx
@@ -4,7 +4,7 @@ import WideCircle from './wide-circle'
 
 const HowSection = () => {
     return (
-        <section className="flex flex-col gap-6 items-center justify-between h-[110dvh] overflow-hidden max-w-7xl mx-auto pb-32 ">
+        <section id='howworks' className="flex flex-col gap-6 items-center justify-between h-[110dvh] overflow-hidden max-w-7xl mx-auto pb-32 ">
             <h3 className="text-[14px] font-semibold text-center text-transparent bg-clip-text bg-gradient-to-l from-[#0A2141] to-[#55FFFA] border-[1.5px] border-white/15 rounded-full px-[14px] py-[4px]" >
                 WHY WE ARE SPECIAL
             </h3>
